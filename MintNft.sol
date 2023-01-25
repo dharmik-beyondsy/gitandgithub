baf06ba158599e111944c1439567b73c9fa044e6
dharmik beyondsy <beyondsy.dharmik@gmail.com>
2023-01-25 09:57:48 +0530

    initial commit

---
+++ MintNft.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: Unlicence
+pragma solidity ^0.8.4;
+
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+import '@openzeppelin/contracts/utils/Counters.sol';
+
+
+contract MintNft is ERC721, Ownable {
+
+    uint256 public mintPrice;
+    uint256 public totalSupply;
+    uint256 public maxSupply;
+    uint256 public maxPerWallet;
+    bool public isPublicMintEnabled;
+    string internal baseTokenUri;
+    address payable public withdrawWallet;
+    mapping(address => uint256) public walletMints;
+
+    constructor() payable ERC721("BEYONDSY", "BSL"){
+        mintPrice = 0.02 ether;
+        totalSupply = 0;
+        maxSupply = 100;
+        maxPerWallet = 3; 
+        //set withdraw wallet address
+    }
+
+    function setIsPublicMintEnabled(bool isPublicMintEnabled_) external onlyOwner {
+        isPublicMintEnabled = isPublicMintEnabled_;
+    }
+
+    //url that has images --url where images are located
+    function setBaseTokenUri(string calldata baseTokenUri_) external onlyOwner {
+        baseTokenUri = baseTokenUri_;
+    }
+
+    //grab the images //display images on opensea
+    function tokenURI(uint256 tokenId_) public view override returns (string memory) {
+        require(_exists(tokenId_), 'Token does not exist!');
+        return string(abi.encodePacked(baseTokenUri, Strings.toString(tokenId_), ".json"));
+    }
+
+    function withdraw() external onlyOwner{
+        (bool success, ) = withdrawWallet.call{ value: address(this).balance}('');
+        require(success, 'withdraw failed');
+    }
+
+    //mint - most important part
+    function mint(uint256 quantity_) public payable {
+        require(isPublicMintEnabled, 'minting not enabled');
+        require(msg.value == quantity_ * mintPrice, 'wrong mint value');
+        //max supply exceed
+        require(totalSupply + quantity_ <= maxSupply, 'sold out');
+        require(walletMints[msg.sender] + quantity_ <= maxPerWallet, 'exceed max wallet');
+
+        for (uint256 i = 0; i< quantity_; i++) {
+            uint256 newTokenId = totalSupply + 1; 
+            totalSupply++;
+            _safeMint(msg.sender, newTokenId);
+        }
+    }
+
+}
